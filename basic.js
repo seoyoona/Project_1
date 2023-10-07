0239dedffceb1c29bdd43b9b6364adaf827930dc
seoyoona <synsyn0311@gmail.com>
2023-10-08 05:05:36 +0900

    worked on sliders for fuckin 5hours

--- basic.js
+++ basic.js
@@ -1,187 +1,3 @@
-
-
-// announcement
-$('.announcement_slider').bxSlider({
-	auto: true,
-});
-
-
-// SNUE edu
-$('.edu_slider_wrap').bxSlider({
-    minSlides: 4,
-	maxSlides: 4,
-	moveSlides: 1,
-	slideWidth: 800,
-	slideMargin: 10,
-	auto: true,
-    pager: false,
-    controls:false,
-});
-
-// SNUE news
-
-const list = document.querySelector('.slides_container');
-const listLi = document.querySelectorAll('.slides_container li a');
-const show_num = 2;
-const total = listLi.length;
-const li_width = parseInt(getComputedStyle(listLi[0]).width);
-let num = 0;
-
-for(let i = 0; i < show_num; i++){
-    var copyobj = listLi[i].cloneNode(true);
-    list.appendChild(copyobj);
-}
-
-// Copy the first slide and append it to the end
-var firstCopyObj = listLi[0].cloneNode(true);
-list.appendChild(firstCopyObj);
-
-document.querySelector('.control > .right_btn').addEventListener('click', function(e) {
-    e.preventDefault();
-    moveRight();
-});
-
-document.querySelector('.control > .left_btn').addEventListener('click', function(e){
-    e.preventDefault();
-
-    if (num == 0){
-        num=total;
-        list.style.transition ='none';
-        list.style.marginLeft=-li_width * num + 'px';
-   }
-
-   setTimeout(function(){
-       num--;
-       list.style.transition='margin-left .5s'
-       ;
-       list.style.marginLeft=-li_width * num +'px';
-   },50);
-});
-
-function moveRight() {
-    num++;
-    if (num == total+1){ // Notice this change here
-        // Reset to the actual first slide without transition after transition ends.
-        setTimeout(function() {
-            num = 0;
-            list.style.transition ='none';
-            list.style.marginLeft='0px';   
-        }, 500); // Adjust this delay to match your transition duration
-   } else {
-       setTimeout(function(){
-           // This is your original code.
-           // It is executed when it's not transitioning from last slide to the first.
-           list.style.transition='margin-left .5s'
-           ;
-           list.style.marginLeft=-li_width * num +'px';
-      },50);
-   }
-}
-
-// Auto slide every 2 seconds
-let autoSlideIntervalId;
-
-function startAutoSlide() {
-    autoSlideIntervalId = setInterval(moveRight, 2000);
-}
-
-startAutoSlide(); 
-
-// Pause button functionality
-document.querySelector('.control>.pause_btn').addEventListener('click', function(e) {
-    e.preventDefault();
-
-    if (this.dataset.paused === 'true') { 
-        startAutoSlide(); 
-        this.dataset.paused = 'false'; 
-      } else { 
-          clearInterval(autoSlideIntervalId); 
-          this.dataset.paused ='true'; 
-      }
-});
-
-
-
-
-// media
-$('.m_slider').bxSlider({
-    minSlides: 2,
-	maxSlides: 3,
-	moveSlides: 1,
-	slideWidth: 230,
-	slideMargin: 10,
-	auto: true,
-})
-
-
-// quick_top_btn
-$(document).ready(function(){
-    // 첫 번째 li 요소에 'active' 클래스 추가
-    $(".quick_top li:first-child").addClass("active");
-
-    // 각 카테고리 버튼에 클릭 이벤트 핸들러를 설정합니다.
-    $('.quick_top_btn').click(function(e) {
-        e.preventDefault(); // 기본 동작(링크 이동)을 막습니다.
-
-        // 모든 li 요소에서 'active' 클래스 제거
-        $('.quick_top_btn').removeClass('active');
-
-        // 현재 클릭된 li 요소에만 'active' 클래스 추가
-        $(this).addClass('active');
-    });
-});
-
-// quick
-
-$(document).ready(function() {
-    // 모든 하위 ul 요소를 숨김
-    $(".quick_bot").hide();
-    $("#student").show();
-  
-    $(".student").click(function() {
-        $(".quick_bot").hide();
-        $("#student").show();
-    });
-  
-    $(".prof").click(function() {
-        $(".quick_bot").hide();
-        $("#prof").show();
-    });
-  
-    $(".general").click(function() {
-        $(".quick_bot").hide();
-        $("#general").show();
-    });
-  });
-
-$(document).ready(function(){
-    // 모든 하위 ul 요소를 숨김
-    $(".f_nav_dep2").hide();
-  
-    $("li > i.fa-solid.fa-bars").click(function(){
-      // 현재 클릭된 li 요소의 하위 ul 요소를 토글
-      $(this).next(".f_nav_dep2").toggle();
-    });
-  });
-
-//tab_btn
-
-$(document).ready(function(){
-    // 첫 번째 li 요소에 'active' 클래스 추가
-    $(".tab li:first-child").addClass("active");
-
-    // 각 li 요소에 클릭 이벤트 핸들러를 설정합니다.
-    $('.tab li').click(function(e) {
-        e.preventDefault(); // 기본 동작(링크 이동)을 막습니다.
-
-        // 모든 li 요소에서 'active' 클래스 제거
-        $('.tab li').removeClass('active');
-
-        // 현재 클릭된 li 요소에만 'active' 클래스 추가
-        $(this).addClass('active');
-    });
-});
-
 // fullmenu.active
 $(document).ready(function() {
     $('.icon_menu').on('click',function(){
@@ -204,3 +20,276 @@ document.addEventListener('DOMContentLoaded', function() {
         gnbBg.style.display = 'none';
     });
 });
+
+
+// quick
+
+$(document).ready(function() {
+    $(".quick_bot").hide();
+    $("#student").show();
+  
+    $(".student").click(function() {
+        $(".quick_bot").hide();
+        $("#student").show();
+    });
+  
+    $(".prof").click(function() {
+        $(".quick_bot").hide();
+        $("#prof").show();
+    });
+  
+    $(".general").click(function() {
+        $(".quick_bot").hide();
+        $("#general").show();
+    });
+  });
+
+$(document).ready(function(){
+    $(".f_nav_dep2").hide();
+  
+    $("li > i.fa-solid.fa-bars").click(function(){
+      $(this).next(".f_nav_dep2").toggle();
+    });
+  });
+
+  // quick_top_btn
+$(document).ready(function(){
+    $(".quick_top li:first-child").addClass("active");
+
+    $('.quick_top_btn').on('click',function(e) {
+        e.preventDefault(); // 기본 동작(링크 이동)을 막습니다.
+
+        $('.quick_top_btn').removeClass('active');
+
+        $(this).addClass('active');
+    });
+});
+
+//tab_btn
+$(document).ready(function(){
+    $(".tab li:first-child").addClass("active");
+
+    $('.tab li').click(function(e) {
+        e.preventDefault();
+
+        $('.tab li').removeClass('active');
+
+        $(this).addClass('active');
+    });
+});
+
+
+
+// announcement
+// $('.announcement_slider').bxSlider({
+// 	auto: true,
+// });
+
+var announceSlider;
+
+$(document).ready(function() {
+    announceSlider = $('.announcement_slider').bxSlider({
+        auto: true,
+
+		onSliderLoad: function() {
+            $('.news_slider_btn>.left_btn').click(function(e) {
+                e.preventDefault();
+                announceSlider.goToPrevSlide();
+            });
+
+            $('.news_slider_btn>.right_btn').click(function(e) {
+                e.preventDefault();
+                announceSlider.goToNextSlide();
+            });
+
+			$('.news_slider_btn>.pause_btn').click(function(e) {
+				e.preventDefault();
+
+				var isPaused = $(this).data('paused');
+
+				if (isPaused) { 
+					announceSlider.startAuto();
+					$(this).data('paused', false);
+                    $(this).html('<i class="fa-solid fa-pause"></i>');
+				} else { 
+					announceSlider.stopAuto();
+					$(this).data('paused', true);
+                    $(this).html('<i class="fa-solid fa-play"></i>');
+				}
+                
+			});
+		}
+    });
+    
+});
+
+
+// SNUE news
+
+const list = document.querySelector('.slides_container');
+const listLi = document.querySelectorAll('.slides_container li a');
+const show_num = 2;
+const total = listLi.length;
+const li_width = parseInt(getComputedStyle(listLi[0]).width);
+let num = 0;
+
+for(let i = 0; i < show_num; i++){
+    var copyobj = listLi[i].cloneNode(true);
+    list.appendChild(copyobj);
+}
+
+var firstCopyObj = listLi[0].cloneNode(true);
+list.appendChild(firstCopyObj);
+
+document.querySelector('.control > .right_btn').addEventListener('click', function(e) {
+    e.preventDefault();
+    moveRight();
+});
+
+document.querySelector('.control > .left_btn').addEventListener('click', function(e){
+    e.preventDefault();
+
+    if (num == 0){
+        num=total;
+        list.style.transition ='none';
+        list.style.marginLeft=-li_width * num + 'px';
+   }
+
+   setTimeout(function(){
+       num--;
+       list.style.transition='margin-left .5s'
+       ;
+       list.style.marginLeft=-li_width * num + 'px';
+   },50);
+});
+
+function moveRight() {
+    num++;
+    if (num == total+1){ 
+        setTimeout(function() {
+            num = 0;
+            list.style.transition ='none';
+            list.style.marginLeft='0px';   
+        }, 500); 
+   } else {
+       setTimeout(function(){
+           list.style.transition='margin-left .5s'
+           ;
+           list.style.marginLeft=-li_width * num + 'px';
+      },50);
+   }
+}
+
+let autoSlideIntervalId;
+
+function startAutoSlide() {
+    autoSlideIntervalId = setInterval(moveRight, 2000);
+}
+
+startAutoSlide(); 
+
+document.querySelector('.control>.pause_btn').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    if (this.dataset.paused === 'true') { 
+        startAutoSlide(); 
+        this.dataset.paused = 'false'; 
+      } else { 
+          clearInterval(autoSlideIntervalId); 
+          this.dataset.paused ='true'; 
+      }
+});
+
+// SNUE edu
+var slider;
+
+$(document).ready(function() {
+    slider = $('.edu_slider_wrap').bxSlider({
+        minSlides: 4,
+        maxSlides: 4,
+        moveSlides: 1,
+        slideWidth: 800,
+        slideMargin: 10,
+        auto: true,
+        pager: false,
+        controls:false,
+
+		onSliderLoad: function() {
+            $('.snue_control_btn>.left_btn').click(function(e) {
+                e.preventDefault();
+                slider.goToPrevSlide();
+            });
+            $('.snue_control_btn>.right_btn').click(function(e) {
+                e.preventDefault();
+                slider.goToNextSlide();
+            });
+            $('.snue_control_btn>.pause_btn').click(function(e) {
+				e.preventDefault();
+
+				var isPaused = $(this).data('paused');
+
+				if (isPaused) { 
+					slider.startAuto();
+					$(this).data('paused', false);
+                    $(this).html('<i class="fa-solid fa-pause"></i>');
+				} else { 
+					slider.stopAuto();
+					$(this).data('paused', true);
+                    $(this).html('<i class="fa-solid fa-play"></i>');
+				}
+			});
+		}
+    });
+});
+
+
+// media
+
+var mSlider;
+
+$(document).ready(function() {
+    mSlider = $('.m_slider').bxSlider({
+        minSlides: 3,
+        maxSlides: 3,
+        slideWidth: 240,
+        slideMargin: 10,
+        moveSlides: 1,
+        adaptiveHeight: true,
+        auto: true,
+        pager: false,
+        controls:false,
+
+		onSliderLoad: function() {
+            $('.media_control>.left_btn').click(function(e) {
+                e.preventDefault();
+                mSlider.goToPrevSlide();
+            });
+
+            $('.media_control>.right_btn').click(function(e) {
+                e.preventDefault();
+                mSlider.goToNextSlide();
+            });
+
+			$('.media_control>.pause_btn').click(function(e) {
+				e.preventDefault();
+
+				var isPaused = $(this).data('paused');
+
+				if (isPaused) { 
+					mSlider.startAuto();
+					$(this).data('paused', false);
+                    $(this).html('<i class="fa-solid fa-pause"></i>');
+				} else { 
+					mSlider.stopAuto();
+					$(this).data('paused', true);
+                    $(this).html('<i class="fa-solid fa-play"></i>');
+				}
+                
+			});
+		}
+    });
+    
+});
+
+
+
