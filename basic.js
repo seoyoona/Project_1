25c1ff6d0aa4c4afde4958ee5b74f1ef62d4b485
seoyoona <synsyn0311@gmail.com>
2023-10-17 18:00:57 +0900

    to-do for tomorrow : sideNav

--- basic.js
+++ basic.js
@@ -21,6 +21,51 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// popup
+const closeBtn = document.querySelector('.popup>.close_btn');
+  const popup = document.querySelector('.popup');
+  closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+
+// mainVisual
+$(function () {
+	var visual = $('.visual_bg>li'); // 슬라이드 이미지
+	var button = $('#buttonList>li'); // 버튼
+	var leftBtn = $('.btnImg .prev');
+	var rightBtn = $('.btnImg .next');
+	var current = 0; // 현재 보이는 이미지
+	var setIntervalId; // clearInterval을 쓰기 위해 변수명이 필요함
+
+	var counterEl = '<div class="counter">1';
+	$('.main_visual').append(counterEl);
+	var counter = $('.counter');
+
+	timer();
+	function timer() {
+		setIntervalId = setInterval(function () {
+			var prev = visual.eq(current);
+			var pn = button.eq(current);
+			move(prev, 0, '-100%');
+			pn.removeClass('on');
+			current++;
+			if (current == visual.length) current = 0;
+			var next = visual.eq(current);
+			var pnn = button.eq(current);
+			move(next, '100%', 0);
+			pnn.addClass('on');
+			cnt(current);
+		}, 3000);
+	}});
+
+	function move(tg, start, end) {
+		tg.css('left', start).stop().animate({ left: end }, { duration: 500, ease: 'easeOutCubic' });
+	}
+
+	function cnt(num) {
+		counter.html(`${num + 1}`);
+	}
+
 
 // quick
 
